3c5d5e6624a6626c67a835e013521c80abf521a4
DaniAkiode <dannyakiode@hotmail.co.uk>
2021-06-17 17:19:43 +0100

    Trying to detch the image

--- dogapp/src/App.js
+++ dogapp/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  /*const [err, setErr] = useState(null);
+  const [Loaded, setLoaded] = useState(false);
+  const [dogs, setDogs] = useState([]);
+  
+  useEffect()
+
+*/
+
+  const [url, setUrl] = useState('');
+  function getData(){
+    fetch('https://dog.ceo/api/breeds/image/random').then(res=> {
+        if(res.ok){
+          return res.json();
+        }
+        throw new Error('Request Failed');
+
+    },NewError => console.log(NewError.message)
+    ).then(jsonFile=>{
+        console.log(jsonFile)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={url}/>
+        <button onClick={getData}>Click for new picture</button>
     </div>
   );
 }
