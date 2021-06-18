d759ae7f630d608a2fc9f860abca9adeffb47ffb
DaniAkiode <dannyakiode@hotmail.co.uk>
2021-06-18 12:56:11 +0100

    comments

--- dogapp/src/App.js
+++ dogapp/src/App.js
@@ -1,24 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
 function App() {
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState(''); //Initalise the url 
+  
+  //Use getData Function to fetch the api
   function getData(){
     fetch('https://dog.ceo/api/breeds/image/random').then(res=> {
         if(res.ok){
-          return res.json();
+          return res.json();//If the link is valid then return the json file otherwise send error
         }
-        throw new Error('Request Failed');
+        throw new Error('Request has Failed');
 
-    },NewError => console.log(NewError.message)
+    },NewError => console.log(NewError.message) //send error message 
     ).then(jsonFile=>{
-        setUrl(jsonFile.message)
+        setUrl(jsonFile.message)//Set Url varible to the new dog image on the api. "message" is initalised to the link of the dog image in the api, 
+        //not to be confused with the .message on the error handling 
     })
   }
   return (
     <div className="dogMain">
-        <Header/>
+        <Header/> 
         <img className="dogImg" src={url} />
         <button className="btn btn-success" onClick={getData}>Click for new picture</button>
     </div>
